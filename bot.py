755d654a96c41d255f1b808cb75f5e313d59eacb
kingdomguard25 <kingdomguard25@gmail.com>
2026-03-01 10:13:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from datetime import datetime
 import csv
 import io
 
-# Настройка логирования
+# Настройка  логирования
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
